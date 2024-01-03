b07306f6c90e8370d225f03954650ac493ca3b20
GabrielF <gabrielFMendonca@gmail.com>
2024-01-03 20:42:22 -0300

    adicionado função de filtrar

--- ToDoListReact/src/App.jsx
+++ ToDoListReact/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Todo from "./components/Todo"
 import TodoForm from './components/TodoForm'
 import Search from './components/Search'
+import Filter from './components/Filter'
 
 import './App.css'
 
@@ -29,6 +30,8 @@ function App() {
   ]);
 
   const [search, setSearch] = useState("")
+  const [filter, setFilter] = useState ("All")
+  const [sort, setSort] = useState("Asc");
 
   const addTodo = (text, category) => {
     const newTodos = [
@@ -59,9 +62,17 @@ function App() {
   return (
   <div className="app">
     <h1>Lista de Tarefas</h1>
-    <Search search={search} setSearch={setSearch}/>
+    <Search search={search} setSearch={setSearch} />
+    <Filter filter={filter} setFilter={setFilter} />
     <div className="todo-list">
       {todos
+      .filter ((todo) => 
+      filter === "All" 
+      ? true 
+      : filter === "Completed" 
+      ? todo.isCompleted 
+      : !todo.isCompleted
+      )
         .filter((todo) => 
           todo.text.toLowerCase().includes(search.toLowerCase())
       )
